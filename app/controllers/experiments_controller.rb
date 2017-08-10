66848d6e4f50b4eb281ad7db7e729a32d0d68c69
Saham Khozestani <saham.khozestani@att.net>
2017-08-09 21:03:16 -0500

    add new html and new routs

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -2,4 +2,13 @@ class ExperimentsController < ApplicationController
 	def index
 		@experiments = Experiment.all.order('created_at DESC')
 	end
+
+	def new
+		@experiment = Experiment.new
+	end
+
+	private
+		def experiment_params
+			params.require(:experiment).permit(:title)
+		end
 end
