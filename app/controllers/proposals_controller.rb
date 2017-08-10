49963697b353d6a5d2fc754792e3791811412547
Andy Schwartz <email": null>
2017-08-10 10:25:45 -0500

    create instance variables within controllers for Proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,15 +1,26 @@
 class ProposalsController < ApplicationController
 
   def create
+    @proposal = Proposal.new(proposal_params)
+    current_user.proposals << @proposal ? @proposal : redirect_to "new"
   end
 
   def show
+    @proposals = Proposal.find_by(id: params[:id])
   end
 
   def edit
+    @proposal = Proposal.find_by(id: params[:id])
   end
 
   def index
+    @proposals = Proposals.all
+  end
+
+  private
+
+  def proposal_params
+    params.require(:proposal).permit(:title, :summary, :hypothesis)
   end
 
 end
