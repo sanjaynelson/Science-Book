5ab03e67adc4f7993f960bc6396b774b36b9c7e5
qymanab <qymanab@gmail.com>
2017-08-10 16:04:26 -0500

    create and pass tests for comment model and routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'experiments/:id/observations' => 'observations#index'
   post 'experiments/:id/observations' => 'observations#create'
 
+  post 'experiments/:id/comments' => 'comments#create'
+
 end
