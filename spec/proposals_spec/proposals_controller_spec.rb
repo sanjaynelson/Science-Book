504c64ca734a3b3d20bcec1afd2c90436c90d902
Andy Schwartz <email": null>
2017-08-10 10:43:44 -0500

    Add rails-controller-testing to gem file, pass first test for controller

--- spec/proposals_spec/proposals_controller_spec.rb
+++ spec/proposals_spec/proposals_controller_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe ProposalsController, type: :controller do
       get :index
       expect(response.status).to eq (200)
     end
+
+    it "has a collection of proposal objects" do
+      get :index
+      expect(assigns(:proposals)).to all( be_a Proposal)
+    end
   end
 end
