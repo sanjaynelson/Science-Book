38c692d50ddb764b09016be041d9c96498f36809
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-08-10 10:58:49 -0500

    Create User controller with routes, Update routes.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :role)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :users, except: [:show]
   resource :sessions, only: [:new, :destroy, :create]
+  root controller: 'static', action: '/'
 end
