9e935726df57edb3222a8106a41b291d3d88d075
Andy Schwartz <email": null>
2017-08-10 18:14:11 -0500

    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:show]
   resources :sessions, only: [:new, :destroy, :create]
-  root 'main#index'
-
 
   resources :proposals, only: [:index, :new, :show, :update, :edit, :create] do
     resources :experiments
-
-  get 'experiments/:id/observations' => 'observations#index'
-  post 'experiments/:id/observations' => 'observations#create'
-  post 'experiments/:id/comments' => 'comments#create'
-
-  resources :experiments do
-  	resources :procedures
   end
 
   resources :experiments do
     resources :procedures
   end
+
+  get 'experiments/:id/observations' => 'observations#index'
+  post 'experiments/:id/observations' => 'observations#create'
+  post 'experiments/:id/comments' => 'comments#create'
+
   root 'proposals#index'
 end
