3d8986ec2041209ef6f383e08e1fb83a5cd61557
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-08-10 11:44:42 -0500

    Include Bcrypt Gem, correct routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :users, except: [:show]
-  resource :sessions, only: [:new, :destroy, :create]
+  resources :users, except: [:show]
+  resources :sessions, only: [:new, :destroy, :create]
   root 'main#index'
 end
