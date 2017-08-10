2f85028daf2420f8b4b99ecf45ffa26a1d1d300b
qymanab <qymanab@gmail.com>
2017-08-10 17:26:13 -0500

    add title to proposals in spec tests

--- spec/observations/models/observation_spec.rb
+++ spec/observations/models/observation_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe Observation, type: :model do
   let!(:user) {User.create!(username: "user1", email: "u@u.com")}
 
-  let!(:proposal) {Proposal.create!(summary: "Flexitarian kogi lomo. Wolf butcher disrupt cornho...", hypothesis: "We need to copy the neural JSON bandwidth!", user: user)}
+  let!(:proposal) {Proposal.create!(summary: "Flexitarian kogi lomo. Wolf butcher disrupt cornho...", hypothesis: "We need to copy the neural JSON bandwidth!", user: user, title: "Thing")}
 
-  let!(:experiment) {Experiment.create!(results: "Centrifuges are no longer present", conclusion: "Centrifuges = Childhood Whimsy", proposal: proposal)}
+  let!(:experiment) {Experiment.create!(results: "Centrifuges are no longer present", conclusion: "Centrifuges = Childhood Whimsy", proposal: proposal, title: "Exy")}
 
 
   describe "validations" do
