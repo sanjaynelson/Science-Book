cd86289fc8c05f6759d2c879ccd78fad5266f1c2
Andy Schwartz & Saham Khozestani <pair+akiraandy.sahamak@devbootcamp.com>
2017-08-11 11:10:57 -0500

    modify create route for observations controller to add observations to experiments

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -4,20 +4,29 @@ class ObservationsController < ApplicationController
   end
 
   def create
-    p "*************"
-    p "inside observation/create"
-    @experiment = Experiment.find(params[:experiment_id])
-    @observation = Observation.new(observation_params)
-    @observation.user = User.find_by(session[:id])
-    @experiment.observations << @observation
-
-    if @observation.save
-      # redirect_to action: 'index'
-      redirect_to @experiment
-    else
-      render :index, status: 422
+    if params[:experiment_id]
+      @experiment = Experiment.find(params[:experiment_id])
+      @observation = Observation.new(observation_params)
+      @observation.user = User.find_by(session[:id])
+      @experiment.observations << @observation
+      if @observation.save
+        # redirect_to action: 'index'
+        redirect_to @experiment
+      else
+        render :index, status: 422
+      end
+    elsif params[:procedure_id]
+      @procedure = Procedure.find(params[:procedure_id])
+      @observation = Observation.new(observation_params)
+      @observation.user = User.find_by(session[:id])
+      @procedure.observations << @observation
+      if @observation.save
+        # redirect_to action: 'index'
+        redirect_to @procedure
+      else
+        render :index, status: 422
+      end
     end
-
   end
 
   private
