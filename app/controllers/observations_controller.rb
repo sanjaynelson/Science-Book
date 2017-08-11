c86a949e988f8184c3645a3fb156604b2fa01d3a
Andy Schwartz & Saham Khozestani <pair+akiraandy.sahamak@devbootcamp.com>
2017-08-11 16:26:34 -0500

    edit obs controller

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -6,30 +6,17 @@ class ObservationsController < ApplicationController
   end
 
   def create
-    if params[:experiment_id]
-      @experiment = Experiment.find(params[:experiment_id])
-      @observation = Observation.new(observation_params)
-      @observation.user = User.find_by(session[:id])
-      @experiment.observations << @observation
-      if @observation.save
-        # redirect_to action: 'index'
-        redirect_to @experiment
-      else
-        render :index, status: 422
-      end
-    elsif params[:procedure_id]
-      @procedure = Procedure.find(params[:procedure_id])
-      @observation = Observation.new(observation_params)
-      user = User.find_by(session[:id])
-      @procedure.username = user.username
-      @procedure.observations << @observation
-      if @observation.save
-        # redirect_to action: 'index'
-        redirect_to @procedure
-      else
-        render :index, status: 422
-      end
+    @experiment = Experiment.find(params[:id])
+    @observation = Observation.new(observation_params)
+    @observation.user = User.find_by(session[:id])
+    @experiment.observations << @observation
+
+    if @observation.save
+      redirect_to action: 'index'
+    else
+      render :index, status: 422
     end
+
   end
 
   private
